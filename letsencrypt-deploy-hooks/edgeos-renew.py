c03979bb779ec7effbcaaed9fc49d5f6e51a0732
Jason Costomiris <jcostom@jasons.org>
2023-03-04 11:42:56 -0500

    add edgeos renewal-hook script

---
+++ letsencrypt-deploy-hooks/edgeos-renew.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import logging
+import tempfile
+import os
+from paramiko import AuthenticationException, SSHClient, AutoAddPolicy, SSHException  # noqa: E501
+from scp import SCPClient
+
+# set to keyfile or password
+AUTH_METHOD = 'password'
+AUTH_USER = 'my-username'
+AUTH_PASSWORD = 'my-password'
+AUTH_KEYFILE = ''
+AUTH_PASSPHRASE = ''
+DEVICE = 'my-router-hostname'
+KNOWN_HOSTS = '/var/tmp/letsedgeos'
+CERT_DIR = '/config/auth'
+CERT_FILE = f'{CERT_DIR}/lets.pem'
+RESTART_COMMAND = 'systemctl restart lighttpd'
+BACKUP_COMMAND = f'cp {CERT_FILE} {CERT_FILE}.backup'
+LE_DIR = '/etc/letsencrypt/live'
+DEBUG = 0
+
+# Setup logger
+logger = logging.getLogger()
+ch = logging.StreamHandler()
+if DEBUG:
+    logger.setLevel(logging.DEBUG)
+    ch.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+    ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(message)s',
+                              datefmt='[%d %b %Y %H:%M:%S %Z]')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def main() -> None:
+    # construct the cert file
+    le_cert_file = f'{LE_DIR}/{DEVICE}/fullchain.pem'
+    le_key_file = f'{LE_DIR}/{DEVICE}/privkey.pem'
+    with open(le_cert_file, "r") as f:
+        le_cert = f.read()
+
+    with open(le_key_file, "r") as f:
+        le_key = f.read()
+
+    lets = le_cert + le_key
+    handle, output = tempfile.mkstemp(suffix='.pem', dir='/var/tmp')
+    with os.fdopen(handle, "w+") as f:
+        f.write(lets)
+
+    # setup ssh/scp connection
+    if not os.path.exists(KNOWN_HOSTS):
+        open(KNOWN_HOSTS, "w+").close()
+        logger.info(f"{KNOWN_HOSTS} did not exist, creating empty file.")
+
+    try:
+        sshclient = SSHClient()
+        sshclient.load_system_host_keys()
+        sshclient.load_host_keys(KNOWN_HOSTS)
+        sshclient.set_missing_host_key_policy(AutoAddPolicy())
+        if AUTH_METHOD == 'password':
+            sshclient.connect(DEVICE, username=AUTH_USER, password=AUTH_PASSWORD)  # noqa: E501
+        else:
+            sshclient.connect(DEVICE, username=AUTH_USER, key_filename=AUTH_KEYFILE, passphrase=AUTH_PASSPHRASE)  # noqa: E501
+
+        # Backup existing cert file
+        stdin, stdout, stderr = sshclient.exec_command(BACKUP_COMMAND)
+        logger.info(f'STDOUT:{stdout.read().decode("utf8")}')
+        logger.info(f'STDERR:{stderr.read().decode("utf8")}')
+        logger.info(f'Return code: {stdout.channel.recv_exit_status()}')
+
+        # transfer temp cert file
+        scp = SCPClient(sshclient.get_transport())
+        scp.put(output, remote_path='/var/tmp')
+
+        # copy tempfile into position
+        stdin, stdout, stderr = sshclient.exec_command(f'cp {output} {CERT_FILE}')  # noqa: E501
+        logger.info(f'STDOUT:{stdout.read().decode("utf8")}')
+        logger.info(f'STDERR:{stderr.read().decode("utf8")}')
+        logger.info(f'Return code: {stdout.channel.recv_exit_status()}')
+
+        # set file permissions
+        stdin, stdout, stderr = sshclient.exec_command(f'chmod 400 {CERT_FILE}')  # noqa: E501
+        logger.info(f'STDOUT:{stdout.read().decode("utf8")}')
+        logger.info(f'STDERR:{stderr.read().decode("utf8")}')
+        logger.info(f'Return code: {stdout.channel.recv_exit_status()}')
+
+        # delete tempfile
+        stdin, stdout, stderr = sshclient.exec_command(f'rm {output}')
+        logger.info(f'STDOUT:{stdout.read().decode("utf8")}')
+        logger.info(f'STDERR:{stderr.read().decode("utf8")}')
+        logger.info(f'Return code: {stdout.channel.recv_exit_status()}')
+
+        # restart lighttpd
+        stdin, stdout, stderr = sshclient.exec_command(f"sudo -S -p '' {RESTART_COMMAND}")  # noqa: E501
+        stdin.write(AUTH_PASSWORD + "\n")
+        stdin.flush()
+        logger.info(f'STDOUT:{stdout.read().decode("utf8")}')
+        logger.info(f'STDERR:{stderr.read().decode("utf8")}')
+        logger.info(f'Return code: {stdout.channel.recv_exit_status()}')
+
+        # close streams
+        stdin.close()
+        stdout.close()
+        stderr.close()
+    except AuthenticationException:
+        logger.debug("Authentication failed, check credentials.")
+    except SSHException as sshException:
+        logger.debug(f"Unable to establish SSH connection: {sshException}")
+    finally:
+        sshclient.close()
+
+    # cleanup files
+    os.unlink(output)
+    os.unlink(KNOWN_HOSTS)
+
+
+if __name__ == "__main__":
+    main()
